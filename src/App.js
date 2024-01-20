f77d2840a68b83a3bea90b073c325ce93a30367d
rdarshan927 <rdarshan927@gmail.com>
2024-01-20 15:19:44 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Darkmode from './challenge/darkmode';
 import Formvalidator from './challenge/formvalidator';
+import Pixelart from './challenge/pixelart';
+import Simplecalculator from './challenge/simpleclaculator';
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Routes>
           <Route index element={<Darkmode/>} />
           <Route path='/formvalidator' element={<Formvalidator/>} />
+          <Route path='/pixelart' element={<Pixelart/>} />
+          <Route path='/simplecalculator' element={<Simplecalculator/>} />
         </Routes>
       </BrowserRouter>
     </div>
